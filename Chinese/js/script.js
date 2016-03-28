8b1209e716a18a29775f46ab05350bd97eecca23
AlexL70 <alexander.levinson.70@gmail.com>
2016-03-28 13:20:27 +0300

    Lecture 60; make index page main content being loaded dynamically using AJAX request.

--- Chinese/js/script.js
+++ Chinese/js/script.js
@@ -7,3 +7,38 @@ $(function () {		//	Same as document.addEventListener("DomContentLoadedx")
 		}
 	});
 });
+
+(function (global) {
+	var dc = {};
+
+	var homeHtml = "snippets/home-snippet.html";
+
+	//	Convenience function for inserting innerHtml for 'select'
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHtml = html;
+		console.log(html);
+	};
+
+	//	Show loading icon inside element identified by 'selector'
+	var showLoading = function (selector) {
+		var html = "<div class='text-center'><img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+
+	//	On page load (before images or CSS)
+	document.addEventListener("DOMContentLoaded", function (event) {
+
+		//	On first load, show home view
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(
+			homeHtml,
+			function (responseText) {
+				var el = document.querySelector("#main-content");
+				el.innerHTML = responseText;
+			},
+			false);
+	});
+
+	global.$dc = dc;
+})(window);
